ca5aba792d436cd4f7de675ac04a741e71a15133
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-06-11 05:10:41 +0200

    add logger file

--- market.py
+++ market.py
@@ -1,27 +1,19 @@
-import redis
-from time import sleep ,time ,mktime
-import requests
-from contracts import load_contracs
+from time import sleep
 from web3 import Web3
-import logging 
+import requests
+import redis
 import json
+
+from logger import create_logger
 from utils import Utils
 
-logging.basicConfig( 
-    level=logging.WARNING,
-    format='(Market) [%(asctime)-24s] [%(levelname)-8s] [%(lineno)d] | %(message)s',
-    handlers=[
-        logging.FileHandler("debug_market.log"),
-        logging.StreamHandler()
-    ]
-)   
+log = create_logger('market')
 
 utl = Utils()
 
 r = redis.Redis(host=utl.configs['redis_host'] ,port=utl.configs['redis_port'] ,decode_responses=True)
 
 w3 = Web3(Web3.HTTPProvider('https://api.s0.t.hmny.io'))
-
 
 params = {"limit":1,"params":[{"field":"saleprice","operator":">=","value":1000000000000000000},{'field':'network' ,'operator':'=' ,'value':'hmy'}],"offset":0,"order":{"orderBy":"saleprice","orderDir":"asc"}}
 headers = {
@@ -45,17 +37,17 @@ headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'
         }
 
-
 if __name__ == '__main__':
 
-    logging.info('[market.py runing ...]')
+    log.debug('[market.py runing ...]')
     count = 0
     saleprice = 0
+
     while True:
 
         if count >= 60 :
             count = 0
-            logging.info(f'- after 1 hour : hero price [{saleprice}]')
+            log.debug(f'- after 1 hour : hero price [{saleprice}]')
 
         try:
             resp = requests.post('https://us-central1-defi-kingdoms-api.cloudfunctions.net/query_heroes' ,json=params ,headers= headers )
@@ -65,6 +57,6 @@ if __name__ == '__main__':
             r.set('hero:price:latest' , float(saleprice) )
 
         except Exception as e:
-            logging.error(f'!! hero price [{e}]')
+            log.error(f'!! hero price [{e}]')
 
         sleep(60)
