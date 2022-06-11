baa47a0ea697c4db4619131078afef4191721f7b
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-06-11 05:15:20 +0200

    fix bug

--- logger.py
+++ logger.py
@@ -8,7 +8,7 @@ def create_logger(name_log):
 
     logger = logging.getLogger(name_log)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(f'%(filename)s-{name_log} [%(asctime)-24s] [%(levelname)-8s] [%(lineno)d] | %(message)s')
+    formatter = logging.Formatter(f'{name_log} [%(asctime)-24s] [%(levelname)-8s] [%(lineno)d] | %(message)s')
     fh = logging.FileHandler(f'debug_{name_log}.log')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
